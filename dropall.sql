ad68890a8898d5389cdd197d78ca982b8c7e77d9
Songrui Li <li884@purdue.edu>
2018-06-19 00:49:37 -0400

    Create dropall.sql

---
+++ dropall.sql
@@ -0,0 +1,28 @@
+rem EE 562 Project 1
+rem Songrui Li
+rem LI884
+
+
+/* Step1: Drop Table STUDENT. */
+DROP TABLE STUDENT;
+
+/* Step2: Drop Table CLASS. */
+DROP TABLE CLASS;
+
+/* Step3: Drop Table FACULTY. */
+DROP TABLE FACULTY;
+
+/* Step4: Drop Table ENROLLED. */
+DROP TABLE ENROLLED;
+
+/* Step5: Drop Table PREREQUISITE. */
+DROP TABLE PREREQUISITE;
+
+/* Step6: Drop View VIEWA. */
+DROP VIEW VIEWA;
+
+/* Step7: Drop View VIEWB. */
+DROP VIEW VIEWB;
+
+/*END*/
+
